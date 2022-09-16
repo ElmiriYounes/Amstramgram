3d83c2c7460f8ac392f03494c248b757885bdca5
elmiri younes <elmiri.younes@hotmail.com>
2022-09-16 11:18:06 +0200

    basic structure of project

--- view/src/App.tsx
+++ view/src/App.tsx
@@ -1,7 +1,17 @@
 import { FC } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App: FC = () => {
-  return <div>hello</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
