b3e7ec3c4c32fa7a79350c661f686a0c7010f612
ElmiriYounes <elmiri.younes@hotmail.com>
2022-11-04 17:15:18 +0100

    add dbname to connect mongodb

--- database.ts
+++ database.ts
@@ -4,8 +4,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const uri: string = process.env.MONGODB_URL!;
+const dbName = process.env.MONGODB_DBNAME!;
 
 mongoose
-  .connect(uri)
+  .connect(uri, { dbName: dbName })
   .then((db) => console.log("Database connected"))
   .catch((err) => console.error(err));
